3a8082522f7abbfc2c911e2ab3998a7ed6a6dc54
Nyame-Wolf <mumenyam@gmail.com>
2023-03-31 21:22:09 +0300

    Explanations for reduce

--- leaguePoints/leaguePoints.js
+++ leaguePoints/leaguePoints.js
@@ -18,6 +18,37 @@ let points = (games) =>
   }, 0);
 
 let points = (g) => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0);
+/*
+explanation above:
+This is an arrow function in JavaScript that calculates the total points for a 
+team based on an array of games. The input g is an array of arrays, where each 
+sub-array represents a game with the format [x, _, y], where x and y are the scores 
+of the two teams and _ is a placeholder for some irrelevant information.
+
+The reduce method is used to iterate through each game in the g array and accumulate 
+the total points earned by the team. The reduce method takes two arguments: a callback 
+function and an initial value for the accumulator.
+
+The callback function takes two arguments: the accumulator a and the current game array [x, _, y]. 
+The expression (x > y ? 3 : x == y) evaluates to either 3 (if the team won), 1 (if the game was a tie), 
+or 0 (if the team lost), depending on the comparison of the scores x and y. The accumulator a is updated 
+with the points earned in the current game and returned to be used as the accumulator for the next iteration.
+
+The initial value for the accumulator is 0, which means that the points earned in the first game are added 
+to 0 to start with. The final value of the accumulator is returned as the total points earned by the team.
+
+So, a is the accumulator variable that keeps track of the total points earned by the team as the reduce method 
+iterates through the games array.
+
+In the code const points = (g) => g.reduce((a, [x, _, y]) => a + (x > y ? 3 : x == y), 0), the expression x == y 
+evaluates to true if the game is a draw (i.e., both teams have the same number of points) and false otherwise.
+
+In JavaScript, true is coerced to 1 and false is coerced to 0 when used in a numeric context such as addition. 
+Therefore, when x == y is true, it adds 1 to the accumulator a, which is the score for a draw.
+*/
+
+points = (g) =>
+  g.reduce((a, c) => a + (c[0] > c[2] ? 3 : c[0] < c[2] ? 0 : 1), 0);
 
 //test
 /*
--- leaguePoints/league_points.py
+++ leaguePoints/league_points.py
@@ -44,6 +44,28 @@ def points(games):
     return sum([0, 1, 3][1+(g[0] > g[2])-(g[0] < g[2])] for g in games)
 
 
+'''
+Explanation above
+The mathematical expression 1+(g[0] > g[2])-(g[0] < g[2]) is used to calculate 
+the number of points that a team earns in a single game. Here's how it works:
+
+g[0] represents the score of the first team in a game.
+g[2] represents the score of the second team in a game.
+g[0] > g[2] returns True if the first team won the game (i.e. if their score is 
+    greater than the second team's score), otherwise it returns False.
+g[0] < g[2] returns True if the first team lost the game (i.e. if their score is 
+    less than the second team's score), otherwise it returns False.
+(g[0] > g[2])-(g[0] < g[2]) calculates the difference between the two Boolean values. 
+If the first team won, this expression evaluates to 1. If the first team lost, it evaluates to -1. 
+If the game was a tie, it evaluates to 0.
+1+(g[0] > g[2])-(g[0] < g[2]) adds 1 to the result of the previous expression, so that the minimum value 
+that can be returned is 1. This gives us the number of points earned by the first team in the game.
+By using this expression inside a list comprehension, the code creates a list of points earned by 
+the first team in each game. Finally, the sum() function is used to add up all the points in the list and 
+return the total number of points earned by the first team across all games.
+'''
+
+
 def points(games):
     return sum(3 if x > y else 0 if x < y else 1 for x, y in (score.split(":") for score in games))
 
