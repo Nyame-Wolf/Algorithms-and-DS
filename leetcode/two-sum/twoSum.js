096bc9a7a32e8917126804e5cb9b0e97a7f94d4c
Nyame-Wolf <mumenyam@gmail.com>
2023-05-03 15:50:29 +0300

    add two sum soln from leetcode

---
+++ leetcode/two-sum/twoSum.js
@@ -0,0 +1,86 @@
+/* 1. Two Sum
+
+Given an array of integers nums and an integer target, 
+return indices of the two numbers such that they add up to target.
+
+You may assume that each input would have exactly one solution, and 
+you may not use the same element twice.
+
+You can return the answer in any order.
+
+ 
+
+Example 1:
+
+Input: nums = [2,7,11,15], target = 9
+Output: [0,1]
+Explanation: Because nums[0] + nums[1] == 9, we return [0, 1].
+Example 2:
+
+Input: nums = [3,2,4], target = 6
+Output: [1,2]
+Example 3:
+
+Input: nums = [3,3], target = 6
+Output: [0,1]
+ 
+
+Constraints:
+
+2 <= nums.length <= 104
+-109 <= nums[i] <= 109
+-109 <= target <= 109
+Only one valid answer exists.
+ 
+
+Follow-up: Can you come up with an algorithm that is less than O(n2) time complexity?*/
+var twoSum = function (nums, target) {
+  let arr = [];
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (target - nums[i] === nums[j]) {
+        arr.push(i);
+        arr.push(j);
+      }
+    }
+  }
+  return arr;
+};
+
+// best solns:
+let twoSum = function (nums, target) {
+  for (let i = 0; i < nums.length; i++) {
+    const pairNum = target - nums[i];
+    const indexOfNum = nums.indexOf(pairNum);
+
+    if (indexOfNum !== -1 && indexOfNum !== i) {
+      return [i, indexOfNum];
+    }
+  }
+};
+
+var twoSum = function (nums, target) {
+  const mp = {};
+
+  for (let i = 0; i < nums.length; i++) {
+    const diff = target - nums[i];
+
+    if (diff in mp) return [i, mp[diff]];
+
+    mp[nums[i]] = i;
+  }
+};
+
+var twoSum = function (nums, target) {
+  let mp = new Map();
+
+  for (let i = 0; i < nums.length; i++) {
+    let diff = target - nums[i];
+
+    if (mp.has(diff)) {
+      return [i, mp.get(diff)];
+    }
+
+    mp.set(nums[i], i);
+  }
+};
